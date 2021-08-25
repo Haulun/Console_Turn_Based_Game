35c018370aa656a406aeca49d549d0f90be5d51b
g-schaffer <guillaume.schaffer@protonmail.com>
2021-08-25 23:09:35 +0200

    first try

---
+++ entity.cpp
@@ -0,0 +1,38 @@
+#include "entity.hpp"
+/*#include <string>*/
+
+
+using namespace JeuConsole;
+
+Entity::Entity(int life, std::string name, int shield, int atk) {
+	m_life(life);
+	m_name(name);
+	m_shield(shield);
+	m_atk(atk);
+}
+
+Entity::~Entity(){}
+
+void Entity::giveDmg(Entity& cible){
+	cible.takeDmg(m_atk);
+}
+
+void Entity::takeDmg(int atk){
+	m_life -= atk; 
+	if(m_life < 0) {
+		m_life = 0;
+	}	
+}
+
+bool Entity::isDead(){
+	return m_life == 0;
+}
+
+std::string Entity::getName(){
+	return m_name;
+}
+
+int Entity::getLife(){
+	return m_life;
+}
+
--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
+#include "entity.hpp"
+#include <iostream>
+
+using namespace JeuConsole;
+
 int main() {
 
+	Entity e1 = Entity(100, "e1", 5, 10);
+/*	Entity e2 = Entity(80, "e2", 5, 12);
+*/
+/*	while(!e1.isDead() && !e2.isDead()){
+		e1.giveDmg(e2);
+		std::cout << e1.getName() << " attacks " << e2.getName();
+		e2.giveDmg(e1);
+		std::cout << e2.getName() << " attacks " << e1.getName();
+
+		std::cout << "life of " << e2.getName() << " is " << e2.getLife();
+		std::cout << "life of " << e1.getName() << " is " << e1.getLife();
+	}
+
+	std::cout << "end of fight : ";
+	if(e1.isDead()){
+		std::cout << e2.getName() << " won";
+	} else {
+		std::cout << e1.getName() << " won";
+	}*/
 }
