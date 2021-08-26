38d83043a5c529d0f466c5129c8f2f81f5351c67
g-schaffer <guillaume.schaffer@protonmail.com>
2021-08-26 21:19:54 +0200

    err random

--- main.cpp
+++ main.cpp
@@ -4,17 +4,73 @@
 #include "troll.hpp"
 #include "goblin.hpp"
 
+
+#include <cstdlib>
+#include <ctime>
+#include <random>
+
 #include <iostream>
 
 using namespace JeuConsole;
+using namespace std;
 
 int main() {
 
-	Mage robert = Mage("Robert");
-	Knight didier = Knight("Didier");
-	Troll zarkof = Troll("Zarkof");
-	Goblin coffin = Goblin("Coffin");
 
-	
+
+	cout << "Choisissez une classe : \nM pour Mage \nK pour Knight" << endl;			
+	char choice;
+
+	Entity *player(0);
+	Entity *enemy(0);
+
+
+	while(choice != 'm' && choice != 'k'){
+		cin >> choice;
+		if(choice != 'm' && choice != 'k'){
+			cout << "caractère non valide" << endl;
+		}
+	}
+
+	if(choice == 'm'){
+		cout << "Vous avez choisi Merlin le mage !" << endl;
+		player = new Mage("Merlin");
+
+	}
+	else{
+		cout << "Vous avez choisi Arthur le chevalier !" << endl;
+		player = new Knight("Arthur");
+	}
+
+	bool isEnemyDead = true;
+
+	while(!player->isDead()){
+
+		if(isEnemyDead){
+			int rand = rand() % 2;
+			if(rand == 0){
+				enemy = new Troll("Troll"); 
+			}
+			else{
+				enemy = new Goblin("Goblin");
+			}
+			isEnemyDead = false;
+
+		}
+
+		//on attaque
+
+		// si l'enmi est mort recréer un enmi
+
+		// sinon l'enmi atk
+
+
+
+
+	}
+
+
+
+	// todo : penser à delete *player / *enemy
 
 }
